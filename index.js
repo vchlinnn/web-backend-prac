95d4393bf6ad4e68d62f2c92f44e3c4c2e7aea87
vchlinnn <chuclinh190305@gmail.com>
2024-07-16 23:14:07 -0500

    Annotated web server

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// import node's built in web server module - import http from 'http'
 const http = require('http')
 
+// create a new web server
 const app = http.createServer((request, response) => {
+    // event handler - called everytime an HTTP request is made 
   response.writeHead(200, { 'Content-Type': 'text/plain' })
   response.end('Hello World')
 })
 
+// listen to HTTP requests sent to port 3001
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
