3f0a9b30e749646c6f460d0bcf2f12e8fcb8aaef
vchlinnn <chuclinh190305@gmail.com>
2024-07-17 21:45:33 -0500

    Create route for deleting resources

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
