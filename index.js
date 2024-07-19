dfce01720b87db24056ea479f8bf0e2fbb969946
vchlinnn <chuclinh190305@gmail.com>
2024-07-18 23:31:08 -0500

    Add deleting and updating functionalities

--- index.js
+++ index.js
@@ -68,11 +68,27 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -132,4 +148,3 @@ mongoose.connect(url)
   
   // this has to be the last loaded middleware, also all the routes should be registered before this!
   app.use(errorHandler)
-  
