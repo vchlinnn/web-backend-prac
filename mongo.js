d1fd8615c2b971ad9bfc55419df757110a3e45ec
vchlinnn <chuclinh190305@gmail.com>
2024-07-18 22:05:55 -0500

    Annotated mongodb prac

--- mongo.js
+++ mongo.js
@@ -7,6 +7,7 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
+// establish connection to the database
 const url =
   `mongodb+srv://chuclinh190305:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
 
@@ -14,6 +15,7 @@ mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
 
+// define the schema for a note and the matching model 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
@@ -21,12 +23,14 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+// create a new note
 const note = new Note({
   content: 'HTML is easy',
   important: true,
 })
 
+// save the object to the database
 note.save().then(result => {
   console.log('note saved!')
-  mongoose.connection.close()
+  mongoose.connection.close() // close the database connection
 })
